d6c2fe7e336fd3b57638eedf7cbbbaab4fa1070d
AAslsop12 <aimee.johansen33@gmail.com>
2018-08-02 08:50:40 -0600

    styled header and navbar components

--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -9,7 +9,7 @@ class Navbar extends Component {
                 {
                     this.props.navbarLinks.map((link, index) => {
                         return (
-                            <a className='navbar__link' key={index} onClick={() => console.log('tryna switch tab')}>
+                            <a className={`navbar__link ${link.active ? 'green-text' : ''}`} key={index} onClick={() => console.log('tryna switch tab')}>
                                 {link.title}
                             </a>
                         )
