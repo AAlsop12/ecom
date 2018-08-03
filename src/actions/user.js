fe2e740626e30f26b0d1ef0ab4c4b74008e2906b
AAslsop12 <aimee.johansen33@gmail.com>
2018-08-02 23:16:10 -0600

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,14 +8,94 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase 1',
-                amount: .55
+                total: 4.90,
+                orderNumber: 'AE309AGNO',
+                orderDate: new Date(),
+                creditCard: '0000',
+                user: {
+                    name: 'Phil Greenwood',
+                    shippingAddress: '1234 West State Street'
+                }
             },
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 4.90
+                total: 6.39,
+                orderNumber: 'AE309AGNO',
+                orderDate: new Date(),
+                creditCard: '0000',
+                user: {
+                    name: 'Angela Ludlow',
+                    shippingAddress: '1234 West State Street'
+                }
             },
+            {
+                _id: 2,
+                total: 99.80,
+                orderNumber: 'AE309AGNO',
+                orderDate: new Date(),
+                creditCard: '0000',
+                user: {
+                    name: 'Crumbles Swallow',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 3,
+                total: 4.90,
+                orderNumber: 'AE309AGNO',
+                orderDate: new Date(),
+                creditCard: '0000',
+                user: {
+                    name: 'armstead down',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 4,
+                total: 4.90,
+                orderNumber: 'AE309AGNO',
+                orderDate: new Date(),
+                creditCard: '0000',
+                user: {
+                    name: 'chloe smalls',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 5,
+                total: 4.90,
+                orderNumber: 'AE309AGNO',
+                orderDate: new Date(),
+                creditCard: '0000',
+                user: {
+                    name: 'Franz Nesnahoj',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 6,
+                total: 4.90,
+                orderNumber: 'AE309AGNO',
+                orderDate: new Date(),
+                creditCard: '0000',
+                user: {
+                    name: 'Clayton Menace',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 7,
+                total: 4.90,
+                orderNumber: 'AE309AGNO',
+                orderDate: new Date(),
+                creditCard: '0000',
+                user: {
+                    name: 'Penelope Jarvis',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            
+
         ]
     })
 }
