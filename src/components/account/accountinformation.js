8596b6e98cb473222f8d595a2d1825c3d6170cd0
AAslsop12 <aimee.johansen33@gmail.com>
2018-08-02 19:38:06 -0600

    developed navbar and header actions for setting new links

--- src/components/account/accountinformation.js
+++ src/components/account/accountinformation.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 class AccountInformation extends Component {
     render() {
+        return (
         <div>
             account info
         </div>
+        )
     }
 }
 
